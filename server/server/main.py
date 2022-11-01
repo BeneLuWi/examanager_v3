e101392722eec7f2e9f2dd2ea015b315f7789b47
David kaub <david.kaub@dfki.de>
2022-11-01 12:12:39 +0100

    Update api docs

--- server/server/main.py
+++ server/server/main.py
@@ -4,6 +4,7 @@ import os
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from starlette.staticfiles import StaticFiles
 
 from server.server.api.api_v1.routers.auth_api import init_admin_user
@@ -13,6 +14,28 @@ from server.server.config import ExamManagerSettings
 settings = ExamManagerSettings()
 logging.basicConfig(level=settings.LOGGING_LEVEL)
 logger = logging.getLogger(settings.APP_NAME)
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Examanager",
+        version=settings.VERSION,
+        description=settings.api_description,
+        routes=app.routes,
+        terms_of_service=settings.terms_of_service,
+        license_info=settings.license_info,
+        contact=settings.contact,
+        tags=settings.tags_metadata,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        # todo not working with local file and currently only with redoc not with openapi. Open api shema js and css
+        #  could be overridden to achieve this
+        "url": "https://www.dfki.de/fileadmin/user_upload/DFKI/Medien/Logos/Logos_DFKI/DFKI_Logo.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
 
 
 def configure_static(fasts_api_app):
