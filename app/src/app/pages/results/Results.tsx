fa52f13db9b24df47eb3bda6ae11420fe231126f
Benedikt Lüken-Winkels <belu02@dfki.de>
2022-11-05 15:31:57 +0100

    Add Result selection

--- app/src/app/pages/results/Results.tsx
+++ app/src/app/pages/results/Results.tsx
@@ -1,4 +1,10 @@
-import React, { FunctionComponent } from "react"
+import React, { FunctionComponent, useEffect, useState } from "react"
+import { Exam } from "../exams/types"
+import { SchoolClass } from "../school_classes/types"
+import { Col, ListGroup, Row } from "react-bootstrap"
+import axios from "axios"
+import { toast } from "react-toastify"
+import DrawerModal from "../../components/drawer-modal/DrawerModal"
 
 type ResultsProps = {}
 
@@ -8,6 +14,24 @@ const Results: FunctionComponent<ResultsProps> = ({}) => {
    *  Hooks
    *
    *******************************************************************************************************************/
+
+  const [exams, setExams] = useState<Exam[]>()
+  const [school_classes, setSchoolClasses] = useState<SchoolClass[]>()
+
+  const [exam, setExam] = useState<Exam>()
+  const [school_class, setSchoolClass] = useState<SchoolClass>()
+
+  useEffect(() => {
+    axios
+      .get("/api/school_class")
+      .then((res) => setSchoolClasses(res.data))
+      .catch(() => toast("Fehler beim Laden der Schulklassen", { type: "error" }))
+
+    axios
+      .get("/api/exam")
+      .then((res) => setExams(res.data))
+      .catch(() => toast("Fehler beim Laden der Klausuren", { type: "error" }))
+  }, [])
 
   /*******************************************************************************************************************
    *
@@ -21,7 +45,48 @@ const Results: FunctionComponent<ResultsProps> = ({}) => {
    *
    *******************************************************************************************************************/
 
-  return <div>Results</div>
+  return (
+    <>
+      <div className="display-4">Klausurergebnisse</div>
+      <Row>
+        <Col>
+          <ListGroup>
+            {school_classes?.map((s) => (
+              <ListGroup.Item
+                key={s._id}
+                action
+                onClick={() => setSchoolClass(s)}
+                className={`${s._id === school_class?._id && "border border-3 border-primary"}`}
+              >
+                <div>
+                  <div className="fw-bold">{s.name}</div>
+                  {s.description}
+                </div>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+
+        <Col>
+          <ListGroup>
+            {exams?.map((e) => (
+              <ListGroup.Item
+                key={e._id}
+                action
+                onClick={() => setExam(e)}
+                className={`${e._id === exam?._id && "border border-3 border-primary"}`}
+              >
+                <div>
+                  <div className="fw-bold">{e.name}</div>
+                  {e.description}
+                </div>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default Results
