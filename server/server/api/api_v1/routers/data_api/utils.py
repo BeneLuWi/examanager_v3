1ac109a642159e2fa8edc8e771304a9703e0dbdf
David kaub <david.kaub@dfki.de>
2022-11-05 18:45:46 +0100

    Add RatingFactory

--- server/server/api/api_v1/routers/data_api/utils.py
+++ server/server/api/api_v1/routers/data_api/utils.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List
+from typing import List, Dict
 
 from server.api.api_v1.routers.data_api.models import Rating
 from server.config import ExamManagerSettings
@@ -10,23 +10,119 @@ logging.basicConfig(level=settings.LOGGING_LEVEL)
 logger = logging.getLogger(settings.APP_NAME)
 
 
-class DefaultRatingsFactory:
+class RatingsFactory:
+    """
+    Compare: https://de.wikipedia.org/wiki/Vorlage:Punktesystem_der_gymnasialen_Oberstufe
+    and https://notenkurve.de/punkte-in-noten-rechner/ for decimal rating
+    """
+
+    mss_to_text_rating: Dict[int, str] = {
+        0: "Ungenügend",
+        1: "Mangelhaft",
+        2: "Mangelhaft",
+        3: "Mangelhaft",
+        4: "Ausreichend",
+        5: "Ausreichend",
+        6: "Ausreichend",
+        7: "Befriedigend",
+        8: "Befriedigend",
+        9: "Befriedigend",
+        10: "Gut",
+        11: "Gut",
+        12: "Gut",
+        13: "Sehr Gut",
+        14: "Sehr Gut",
+        15: "Sehr Gut",
+    }
+
+    mss_to_school_rating: Dict[int, str] = {
+        0: "6",
+        1: "5-",
+        2: "5",
+        3: "5+",
+        4: "4-",
+        5: "4",
+        6: "4+",
+        7: "3-",
+        8: "3",
+        9: "3+",
+        10: "2-",
+        11: "2",
+        12: "2+",
+        13: "1-",
+        14: "1",
+        15: "1+",
+    }
+
+    mss_to_decimal_rating: Dict[int, float] = {
+        # This could either start at 6.0 and end with 1.0 or start at 6.7 and end with 0.7
+        0: 6.0,
+        1: 5.7,
+        2: 5.3,
+        3: 5.0,
+        4: 4.7,
+        5: 4.3,
+        6: 4.0,
+        7: 3.7,
+        8: 3.3,
+        9: 3.0,
+        10: 2.7,
+        11: 2.3,
+        12: 2.0,
+        13: 1.7,
+        14: 1.3,
+        15: 1.0,
+    }
+
+    mss_to_decimal_rating_alternative: Dict[int, float] = {
+        # This could either start at 6.0 and end with 1.0 or start at 6.7 and end with 0.7
+        0: 5.7,
+        1: 5.3,
+        2: 5.0,
+        3: 4.7,
+        4: 4.3,
+        5: 4.0,
+        6: 3.7,
+        7: 3.3,
+        8: 3.0,
+        9: 2.7,
+        10: 2.3,
+        11: 2.0,
+        12: 1.7,
+        13: 1.3,
+        14: 1.0,
+        15: 0.7,
+    }
+
+    default_percentage_to_mss_points: dict[int, float] = {
+        0: 0,
+        1: 0.20,
+        2: 0.27,
+        3: 0.33,
+        4: 0.39,
+        5: 0.455,
+        6: 0.50,
+        7: 0.55,
+        8: 0.60,
+        9: 0.65,
+        10: 0.70,
+        11: 0.75,
+        12: 0.80,
+        13: 0.85,
+        14: 0.90,
+        15: 0.95,
+    }
+
     def create_default_ratings(self) -> List[Rating]:
-        ratings: List[Rating] = list()
-        ratings.append(Rating(percentage=0.5, mss_points=1))
-        ratings.append(Rating(percentage=0.5, mss_points=2))
-        ratings.append(Rating(percentage=0.5, mss_points=3))
-        ratings.append(Rating(percentage=0.5, mss_points=4))
-        ratings.append(Rating(percentage=0.5, mss_points=5))
-        ratings.append(Rating(percentage=0.5, mss_points=6))
-        ratings.append(Rating(percentage=0.5, mss_points=7))
-        ratings.append(Rating(percentage=0.5, mss_points=8))
-        ratings.append(Rating(percentage=0.5, mss_points=9))
-        ratings.append(Rating(percentage=0.5, mss_points=10))
-        ratings.append(Rating(percentage=0.5, mss_points=11))
-        ratings.append(Rating(percentage=0.5, mss_points=12))
-        ratings.append(Rating(percentage=0.5, mss_points=13))
-        ratings.append(Rating(percentage=0.5, mss_points=14))
-        ratings.append(Rating(percentage=0.5, mss_points=15))
+        ratings: List[Rating] = [
+            Rating(
+                percentage=self.default_percentage_to_mss_points[mss],
+                mss_points=mss,
+                school_rating=self.mss_to_school_rating[mss],
+                text_rating=self.mss_to_text_rating[mss],
+                decimal_rating=self.mss_to_decimal_rating[mss],
+            )
+            for mss in range(16)
+        ]
 
         return ratings
