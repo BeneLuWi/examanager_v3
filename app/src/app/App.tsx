7168e361db29f7fbcd08e07f7d192252cbae5ca8
Benedikt Lüken-Winkels <belu02@dfki.de>
2022-12-15 11:35:04 +0100

    Add React Query and React Hook Forms

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -2,6 +2,15 @@ import React, { FunctionComponent } from "react"
 import { AuthProvider } from "./auth"
 import Routing from "./routing/Routing"
 import { ToastContainer } from "react-toastify"
+import { QueryClient, QueryClientProvider } from "react-query"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 const App: FunctionComponent = ({}) => {
   /*******************************************************************************************************************
@@ -23,10 +32,12 @@ const App: FunctionComponent = ({}) => {
    *******************************************************************************************************************/
 
   return (
-    <AuthProvider>
-      <Routing />
-      <ToastContainer />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Routing />
+        <ToastContainer />
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }
 
